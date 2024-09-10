b75ab88b2d24860d150edad05327b17b59e3db0d
Hy30k92 <nakmh92@gmail.com>
2024-09-10 17:49:34 +0900

    terraform AWS EC2 instance nginx module

---
+++ ec2_instance_nginx/variables.tf
@@ -0,0 +1,24 @@
+variable "ami" {
+  description = "ami id for instance"
+  type = string
+}
+
+variable "instance_type" {
+  description = "type of instance"
+  type = string
+}
+
+variable "key_name" {
+  description = "key_name for instance"
+  type = string
+}
+
+variable "instance_name" {
+  description = "instance name for instance"
+  type = string
+}
+
+variable "security_gorup_id" {
+  description = "security_gorup_id for instance"
+  type = string
+}
