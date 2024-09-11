cb609548dc8fcc3cb94f5e540d54990c848d52c9
Hy30k92 <nakmh92@gmail.com>
2024-09-11 11:46:50 +0900

    Add files via upload

---
+++ ec2_instance_fastapi/main.tf
@@ -0,0 +1,23 @@
+# EC2 모듈 리소스 정의
+
+resource "aws_instance" "this" {
+  ami = var.ami
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  root_block_device {
+    volume_size           = 20
+    volume_type           = "gp3"
+    delete_on_termination = true
+  }
+
+  disable_api_termination = true
+  vpc_security_group_ids = [var.security_gorup_id]
+
+  tags = {
+    Name = var.instance_name
+  }
+
+  user_data = filebase64("${path.module}/user_data.sh")
+
+}
