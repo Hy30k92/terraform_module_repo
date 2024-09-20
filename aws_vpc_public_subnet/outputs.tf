0dfb9e4468f315c5699c3ec30964080f1aee79ce
Hy30k92 <nakmh92@gmail.com>
2024-09-20 10:51:32 +0900

    수정된 outputs.tf

--- aws_vpc_public_subnet/outputs.tf
+++ aws_vpc_public_subnet/outputs.tf
@@ -12,8 +12,8 @@ output "public_igw_id" {
 }
 
 
-# output "public_sg_id" {
-#   value = aws_security_group.public_sg.id
-# }
+output "public_sg_id" {
+  value = aws_security_group.public_sg.id
+}
 
 
